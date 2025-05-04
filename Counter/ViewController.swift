c824494c997f50082b7f9837867eb6642d99f32f
овощ <prostoroker@mail.ru>
2025-05-05 00:31:59 +0300

    Создан таки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Counter: UILabel!
+    
+    @IBOutlet weak var tap: UIButton!
+    
+    var number = 1
+    @IBAction func taping(_ sender: Any) {
+        
+        Counter.text = "Значение счетчика: \(number)"
+        return number += 1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Counter.text = "Значение счетчика:0"
+        tap.setTitle("Нажми на меня",for:.normal)
     }
 
 
